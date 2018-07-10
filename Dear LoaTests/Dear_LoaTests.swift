6e18d2868e8a7fc15e0a1ba03a42ac2c6d8df297
Rob Miguel <rob.m@owletcare.com>
2018-07-10 08:03:21 -0600

    Initial Commit

---
+++ Dear LoaTests/Dear_LoaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dear_LoaTests.swift
+//  Dear LoaTests
+//
+//  Created by Rob Miguel on 7/10/18.
+//  Copyright © 2018 robmgl. All rights reserved.
+//
+
+import XCTest
+@testable import Dear_Loa
+
+class Dear_LoaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
